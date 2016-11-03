9752851d7e1b94eb3989660c88304d2d3b6384c0
Christian Lawson-Perfect <christianperfect@gmail.com>
2016-11-03 09:50:30 +0000

    Heroku: change db settings

--- pretendlti/settings.py
+++ pretendlti/settings.py
@@ -116,8 +116,10 @@ USE_TZ = True
 # Update database configuration with $DATABASE_URL.
 
 DATABASES = {
-    'default': dj_database_url.config(default="postgres:///numbas-lti-provider", conn_max_age=500)
+    'default': {}
 }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
