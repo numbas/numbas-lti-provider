2d506c8da941c52b9555764eb3da195914b95d09
Christian Lawson-Perfect <christianperfect@gmail.com>
2016-12-12 11:20:31 +0000

    fix encoding of SCORM CMI
    When the CMI contains raw HTML in an element's value, it needs to be
    escaped.
    
    This adds a dependency on simplejson, so we can use its "For HTML"
    encoder.

--- numbas_lti/views.py
+++ numbas_lti/views.py
@@ -20,6 +20,7 @@ from channels import Channel
 import datetime
 import csv
 import json
+import simplejson
 import string
 from django.contrib.staticfiles.templatetags.staticfiles import static
 
@@ -668,7 +669,7 @@ class RunAttemptView(generic.detail.DetailView):
 
         scorm_cmi.update(latest_elements)
         
-        context['scorm_cmi'] = json.dumps(scorm_cmi)
+        context['scorm_cmi'] = simplejson.encoder.JSONEncoderForHTML().encode(scorm_cmi)
 
         return context
 
