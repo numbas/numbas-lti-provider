7572ea20b7c5af2bc76a567581fec9f5860f1f7b
Christian Lawson-Perfect <christianperfect@gmail.com>
2017-06-05 11:52:14 +0100

    add debug toolbar urls if available

--- pretendlti/urls.py
+++ pretendlti/urls.py
@@ -22,3 +22,12 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('numbas_lti.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    try:
+        import debug_toolbar
+        urlpatterns = [
+            url(r'^__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
+    except ImportError:
+        pass
