881c0cd2f68bb380297d90c9b3ab9b5c7e1441e8
Christian Lawson-Perfect <christianperfect@gmail.com>
2020-02-17 14:46:37 +0000

    first_setup.py: save email settings

--- first_setup.py
+++ first_setup.py
@@ -175,6 +175,8 @@ class Command(object):
             (r"^CHANNEL_LAYERS = {.*?^}", set_channel_layers),
             (r"^SECRET_KEY = '()'", 'SECRET_KEY'),
             (r"^ALLOWED_HOSTS = \['(localhost)'\]", 'ALLOWED_HOSTS'),
+            (r"EMAIL_COMPLETION_RECEIPTS = (True)", "EMAIL_COMPLETION_RECEIPTS"),
+            (r"DEFAULT_FROM_EMAIL = ('')", "DEFAULT_FROM_EMAIL"),
         ]
         self.sub_file('numbasltiprovider/settings.py', settings_subs)
 
