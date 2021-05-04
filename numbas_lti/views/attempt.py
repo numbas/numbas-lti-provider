7a5420bec9eeaf0a6f667d6863cace5d9f0b1df7
Christian Lawson-Perfect <christianperfect@gmail.com>
2021-05-04 08:19:19 +0100

    reverse the order of SCORM elements in the timeline view
    I missed this when changing the default order of SCORM elements

--- numbas_lti/views/attempt.py
+++ numbas_lti/views/attempt.py
@@ -162,7 +162,7 @@ class AttemptTimelineView(MustHaveExamMixin,MustBeInstructorMixin,ResourceManage
         context = super().get_context_data(*args,**kwargs)
 
         context['resource'] = self.object.resource
-        context['elements'] = [e.as_json() for e in resolve_diffed_scormelements(self.object.scormelements.all())]
+        context['elements'] = [e.as_json() for e in resolve_diffed_scormelements(self.object.scormelements.reverse())]
         context['launches'] = [l.as_json() for l in self.object.launches.all()]
 
         return context
