d102c3c49e47460246071e892855ceb8a45216d2
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-10-04 10:37:25 +0100

    Fix Attempt.completed when the due date hasn't passed yet

--- numbas_lti/models.py
+++ numbas_lti/models.py
@@ -1328,7 +1328,7 @@ class Attempt(models.Model):
             True if:
                 * completion_status = 'completed'
                 * or the resource is not available to the student
-                * or the student has not re-opened the attempt
+                * or the due date has passed and the student has not re-opened the attempt
         """
 
         if self.completion_status == 'completed':
@@ -1336,6 +1336,10 @@ class Attempt(models.Model):
 
         if not self.resource.is_available(self.user):
             return True
+
+        availability = self.resource.available_for_user(self.user)
+        if availability.due_date is None or availability.due_date > timezone.now():
+            return False
 
         return not self.student_has_reopened()
 
