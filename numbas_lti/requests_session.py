caf338f141e6493523ce7635ca2d455a086af9c6
Christian Lawson-Perfect <christian@lawson-perfect.uk>
2025-10-23 14:17:09 +0100

    try setting a timeout for requests
    maybe this will prevent the daphne-hanging bug?

--- numbas_lti/requests_session.py
+++ numbas_lti/requests_session.py
@@ -2,8 +2,13 @@ from . import version
 from django.conf import settings
 import requests
 
+class Session(requests.Session):
+    def send(self, request, **kwargs):
+        kwargs.setdefault('timeout', (5,30))
+        return super().send(request, **kwargs)
+
 def get_session():
     REQUESTS_USER_AGENT = getattr(settings, 'REQUESTS_USER_AGENT', 'Numbas LTI provider')
-    session = requests.Session()
+    session = Session()
     session.headers['User-Agent'] = f'{REQUESTS_USER_AGENT} {version}'
     return session
