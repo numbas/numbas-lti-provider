0c89fd31e78d8edfce702e6816cfbcc43de0ca21
Christian Lawson-Perfect <christian@lawson-perfect.uk>
2025-11-13 11:26:58 +0000

    redis debug requests

--- numbasltiprovider/asgi.py
+++ numbasltiprovider/asgi.py
@@ -1,11 +1,16 @@
 import os
 
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+from .debug_handler import DebugASGIHandler
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "numbasltiprovider.settings")
-django_asgi_app = get_asgi_application()
+
+django.setup(set_prefix=False)
+django_asgi_app = DebugASGIHandler()
 
 import numbas_lti.routing
 
