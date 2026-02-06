b30fa888c91388dc8e490b4313be748c337f6eff
Christian Lawson-Perfect <christian@lawson-perfect.uk>
2026-02-06 11:21:57 +0000

    context summary: cope with max_score 0

--- numbas_lti/views/context.py
+++ numbas_lti/views/context.py
@@ -153,7 +153,7 @@ class ContextSummaryView(generic.detail.DetailView):
             context['total_score'] = 0
             context['max_score'] = 1
 
-        context['scaled_score'] = context['total_score'] / context['max_score']
+        context['scaled_score'] = context['total_score'] / context['max_score'] if context['max_score'] != 0 else 0
 
         context.update({
             'resource_groups': resource_groups,
