4069d444f138b0600efab86e1055e2e87d8aae41
Christian Lawson-Perfect <christian@lawson-perfect.uk>
2026-07-16 12:40:45 +0100

    fix LTIUserData.get_source_id
    There was a reference to `self.lti_13`, whihc doesn't exist - I must
    have left it in after a refactor. That line could only be reached if
    there is an `LTI_11_UserData` record but with a blank
    `lis_person_sourcedid`.

--- numbas_lti/models.py
+++ numbas_lti/models.py
@@ -1067,10 +1067,8 @@ class LTIUserData(models.Model):
 
     def get_source_id(self):
         try:
-            if self.lti_11 and self.lti_11.lis_person_sourcedid:
+            if self.lti_11.lis_person_sourcedid:
                 return self.lti_11.lis_person_sourcedid
-            elif self.lti_13 and self.lti_13.lis_person_sourcedid:
-                return self.lti_13.lis_person_sourcedid
             else:
                 return self.consumer_user_id
         except LTI_11_UserData.DoesNotExist:
